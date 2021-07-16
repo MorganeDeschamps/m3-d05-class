e687b69e2cafd3770897c4c4d8a895e3c9c71c46
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-07-16 19:05:02 +0200

    created workspaces

--- __instructor_code__/01_code-react_routing_intro/src/App.js
+++ __instructor_code__/01_code-react_routing_intro/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter,  Route, Link, Switch } from 'react-router-dom';
+
+import HomePageView from './views/HomePage';
 import FirstPageView from './views/FirstPage';
 import SecondPageView from './views/SecondPage';
 
 function App() {
   return (
     <div className='App'>
-       <h1>Hello Pizza Bytes</h1>
+      
+      <h1>Hello Pizza Bytes</h1>
+      
+      <BrowserRouter>
+
+        <nav>
+          <span>
+            <Link to='/first-page'>Link to first-page</Link>
+          </span>
+          <span>
+            <Link to='/second-page/jose'>Jose's Profile</Link>
+          </span>
+          <span>
+            <Link to='/second-page/santi'>Santi's Profile</Link>
+          </span>
+        </nav>
+
+        <Switch>
+      
+          <Route path='/second-page/:name'  component={SecondPageView}/>
+          <Route path='/first-page'  component={FirstPageView}/>
+
+          <Route path='/' component={HomePageView}/>
+
+        </Switch>
+      
+      </BrowserRouter>
+    
     </div>
   );
 }
